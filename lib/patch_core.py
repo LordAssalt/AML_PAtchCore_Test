6ed6c998f09af27fbd5b559c985ac0fb003bca1d
Alex Carluccio <alexcarluccio@live.it>
2023-01-07 15:15:17 +0100

    Update patch_core.py

--- lib/patch_core.py
+++ lib/patch_core.py
@@ -137,7 +137,7 @@ class PatchCore(torch.nn.Module):
             img = transform(segm_map)
             img.save(f"{number}segmap_out.jpg")
 
-            img = transform(torch.squeeze(sample))
+            img = transform(torch.squeeze(sample).transpose(0, 2))
             img.save(f"{number}sample.jpg")
 
             img = transform(torch.squeeze(mask))
